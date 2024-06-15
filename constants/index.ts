ea29f22ecf4706bd571483f7c0c449950966a605
Phani <mysteriousphani@gmail.com>
2024-06-15 13:33:48 +0530

    Add people profiles to landing page

--- constants/index.ts
+++ constants/index.ts
@@ -1,149 +1,246 @@
-export const navLinks  = [
-    {
-        label: "Home", 
-        route: "/"
-    },
-    {
-        label: "Our Hosts",
-        route: "/hosts"
-    },
-]
-
+export const navLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Our Hosts",
+    route: "/hosts",
+  },
+];
 
 export const footerLinks = [
-    {
-        label: "Home",
-        route: "/"
-    },
-    {
-        label: "About Us",
-        route: "/about"
-    },
-    {
-        label: "Features",
-        route: "/features"
-    },
-    {
-        label: "Pricing",
-        route: "/pricing"
-    },
-    {
-        label: "Testimonials",
-        route: "/testimonials"
-    },
-    {
-        label: "Privacy Policy",
-        route: "/privacy-policy"
-    }
-]
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "About Us",
+    route: "/about",
+  },
+  {
+    label: "Features",
+    route: "/features",
+  },
+  {
+    label: "Pricing",
+    route: "/pricing",
+  },
+  {
+    label: "Testimonials",
+    route: "/testimonials",
+  },
+  {
+    label: "Privacy Policy",
+    route: "/privacy-policy",
+  },
+];
 
 export const LandingPageProfessions = [
-    "Influencer",
-    "Companion",
-    "Model",
-    "Fashion Designer",
-    "Sales Executive",
-    "More"
-]
+  "Influencer",
+  "Companion",
+  "Model",
+  "Fashion Designer",
+  "Sales Executive",
+  "More",
+];
 
 export const LandingPageProfiles = [
-    {
-        profession: "Influencer",
-        profiles: [
-            {
-                name: "Megha",
-                username: "@megha_24",
-                location: "Bhopal",
-                description: "Hey I am megha and I am intrestd into travel and music.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/megha.jpg"
-            },
-            {
-                name: "Rashmi",
-                location: "Mohali",
-                username: "@rashmithakur",
-                description: "Hi, My name is Rashmi Thakur, I have done bachelors in journalism.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/rashmi.jpg"
-            },
-            {
-                name: "Dibyendu Banerjee",
-                username: "@deep105",
-                location: "Mumbai",
-                description: "Hi, I am Dibyendu Banerjee and I am a Professor and musician.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/deep.jpg"
-            },
-            {
-                name: "Amaan",
-                username: "@nizami",
-                location:"Ahmedabad",
-                description: "I am amaan and I am a Travel Assistant and Influencer.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/amaan.jpg"
-            }
-        ]
-    },
-    {
-        profession: "Companion",
-        profiles: [
-            {
-                name: "Megha",
-                username: "@megha_24",
-                location: "Bhopal",
-                description: "Hey I am megha and I am intrestd into travel and music.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/megha.jpg"
-            },
-            {
-                name: "Rashmi",
-                location: "Mohali",
-                username: "@rashmithakur",
-                description: "Hi, My name is Rashmi Thakur, I have done bachelors in journalism.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/rashmi.jpg"
-            },
-            {
-                name: "Dibyendu Banerjee",
-                username: "@deep105",
-                location: "Mumbai",
-                description: "Hi, I am Dibyendu Banerjee and I am a Professor and musician.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/deep.jpg"
-            },
-            {
-                name: "Amaan",
-                username: "@nizami",
-                location:"Ahmedabad",
-                description: "I am amaan and I am a Travel Assistant and Influencer.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/amaan.jpg"
-            }
-        ]
-    },
-    {
-        profession: "Model",
-        profiles: [
-            {
-                name: "Megha",
-                username: "@megha_24",
-                location: "Bhopal",
-                description: "Hey I am megha and I am intrestd into travel and music.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/megha.jpg"
-            },
-            {
-                name: "Rashmi",
-                location: "Mohali",
-                username: "@rashmithakur",
-                description: "Hi, My name is Rashmi Thakur, I have done bachelors in journalism.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/rashmi.jpg"
-            },
-            {
-                name: "Dibyendu Banerjee",
-                username: "@deep105",
-                location: "Mumbai",
-                description: "Hi, I am Dibyendu Banerjee and I am a Professor and musician.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/deep.jpg"
-            },
-            {
-                name: "Amaan",
-                username: "@nizami",
-                location:"Ahmedabad",
-                description: "I am amaan and I am a Travel Assistant and Influencer.",
-                image: "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/amaan.jpg"
-            }
-        ]
-    },
-]
+  {
+    profession: "Influencer",
+    profiles: [
+      {
+        name: "Megha",
+        username: "@megha_24",
+        location: "Bhopal",
+        description: "Hey I am megha and I am intrestd into travel and music.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/megha.jpg",
+      },
+      {
+        name: "Rashmi",
+        location: "Mohali",
+        username: "@rashmithakur",
+        description:
+          "Hi, My name is Rashmi Thakur, I have done bachelors in journalism.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/rashmi.jpg",
+      },
+      {
+        name: "Dibyendu Banerjee",
+        username: "@deep105",
+        location: "Mumbai",
+        description:
+          "Hi, I am Dibyendu Banerjee and I am a Professor and musician.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/deep.jpg",
+      },
+      {
+        name: "Amaan",
+        username: "@nizami",
+        location: "Ahmedabad",
+        description: "I am amaan and I am a Travel Assistant and Influencer.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/amaan.jpg",
+      },
+    ],
+  },
+  {
+    profession: "Companion",
+    profiles: [
+      {
+        name: "Piyush Panwar",
+        username: "@piyushpanwar",
+        location: "Bisrakh Jalapur",
+        description: "Hey I am piyush and I am passionate and hardworking.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/piyush.jpg",
+      },
+      {
+        name: "Kumkum Jain",
+        location: "Jaipur",
+        username: "@_kumkum_ ",
+        description: "Hi, I am Kumkum Jain and I work as a Finance Associate.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/kumkum.jpg",
+      },
+      {
+        name: "Nitesh Uttekar",
+        username: "@nitesh",
+        location: "Mumbai",
+        description: "Hi, I am Nitesh Uttekar and I am a Rapper.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/nitesh.jpg",
+      },
+      {
+        name: "Ashwini",
+        username: "@ashwinipatil",
+        location: "Uran Islampur",
+        description:
+          "I am Ashwini, a Software Engineer worked as web developer.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/ashwini.jpg",
+      },
+    ],
+  },
+  {
+    profession: "Model",
+    profiles: [
+      {
+        name: "Deeps",
+        username: "@divinesoul1111",
+        location: "Hyderabad",
+        description: "Hey I am Deeps and I am a professional Model and Author.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/deeps.jpg",
+      },
+      {
+        name: "Yograj Shukla",
+        location: "Delhi",
+        username: "@yigishukla",
+        description: "Hi, I am yograj shukla and I am a Fashion Model.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/yograj.jpg",
+      },
+      {
+        name: "Arzoo",
+        username: "@arzoo_dhanda",
+        location: "Ganaur",
+        description:
+          "I am here to know about you and help you improve yourself.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/arzoo.jpg",
+      },
+      {
+        name: "Thiru Selvam",
+        username: "@mr.thiru",
+        location: "Chennai",
+        description: "I am Thiru Selvann and Acting is my passion.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/thiruselvam.jpg",
+      },
+    ],
+  },
+  {
+    profession: "Fashion Designer",
+    profiles: [
+      {
+        name: "Pruthvi",
+        username: "@pruthvi",
+        location: "Nagpur",
+        description:
+          "Hey I am Pruthvi, a fashion Designer and a small business owner specializing in sustainable and slow fashion.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/pruthvi.jpg",
+      },
+      {
+        name: "Bhavya Sri",
+        location: "Benguluru",
+        username: "@bhavyasri",
+        description:
+          "Hi, I am Bhavya sri and I have a keen eye on work efficiency.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/bhavyasri.jpg",
+      },
+      {
+        name: "Bichin Chandran",
+        username: "@sebic",
+        location: "Kollam",
+        description:
+          "I am a simple human with an open minded and also god blessed me with some sort of skills like Designing, Illustration, Dancing, Acting.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/bichin.jpg",
+      },
+      {
+        name: "Gnana Bharat",
+        username: "@gnanabharat",
+        location: "Benguluru",
+        description:
+          "Dynamic Fashion Designer blending innovation with timeless elegance. Commited to individuality and self expression.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/gnanabharat.jpg",
+      },
+    ],
+  },
+  {
+    profession: "Sales Executive",
+    profiles: [
+      {
+        name: "Devesh",
+        username: "@devesh1234",
+        location: "Benguluru",
+        description:
+          "I am dedicated to driving revenue growth and exceeding sales targets through strategic planning and relationship building.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/devesh.jpg",
+      },
+      {
+        name: "Rijo John",
+        location: "Delhi",
+        username: "@john",
+        description:
+          "I am customer success manager by profession and traveller by passion.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/john.jpg",
+      },
+      {
+        name: "Shruthi Pawar",
+        username: "@shruthisp06",
+        location: "Bhukum",
+        description:
+          "I am Shruthi Pawar, a Sales executive and a good listner.",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/shruthi.jpg",
+      },
+      {
+        name: "Satya",
+        username: "@satyag",
+        location: "Benguluru",
+        description:
+          "Sell softwares by day and meeting intresting people by the night!",
+        image:
+          "https://goformeet.s3.ap-south-1.amazonaws.com/landingPageImages/satya.jpg",
+      },
+    ],
+  },
+];
