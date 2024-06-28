f97dd0b81c425170d7cb64e2b3311da1fb0c032e
Phani <mysteriousphani@gmail.com>
2024-06-28 13:53:53 +0530

    Add DataTable

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { User, userColumns } from './columns'
+import axios from 'axios'
+import { DataTable } from './data-table'
 
-const Users = () => {
+async function getData(): Promise<User[]> {
+  const apiUrl = `${process.env.BACKEND_URL}/admin/get-users`
+
+  const response = await axios.get(apiUrl)
+  const data = response.data;
+  const modifiedData = data.map((user: User) => {
+    return {
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString(),
+    }
+  })
+  return modifiedData
+}
+ 
+
+const Users = async () => {
+  const data = await getData();
   return (
     <div className='p-5'>
       <h2 className='text-2xl font-bold'>Users</h2>
-      
+      <DataTable columns={userColumns} data={data} />
     </div>
   )
 }
