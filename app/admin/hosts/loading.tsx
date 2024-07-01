470a3d572494dbfa8d2b631092f0c4bbad8fbd1e
Phani <mysteriousphani@gmail.com>
2024-07-02 02:03:07 +0530

    Add loader

---
+++ app/admin/hosts/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Oval } from "react-loader-spinner";
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Oval color="#ee3220" secondaryColor="#ee322040" height={100} width={100} />
+    </div>
+  );
+};
+
+export default Loading;
