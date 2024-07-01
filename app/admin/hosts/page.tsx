f5b58822b18055b5eff287a6c9f2d3f057b31880
Phani <phani@Phanis-MacBook-Air.local>
2024-07-01 10:15:06 +0530

    Reduce Hosts

--- app/admin/hosts/page.tsx
+++ app/admin/hosts/page.tsx
@@ -1,27 +1,26 @@
-"use client";
-
 import React from "react";
-import { Host, User, hostColumns } from "../../../components/ui/columns";
+import { Host, hostColumns } from "../../../components/ui/columns";
 import axios from "axios";
 import { DataTable } from "../../../components/ui/data-table";
 
 async function getData(): Promise<Host[]> {
   const apiUrl = `${process.env.BACKEND_URL}/admin/get-hosts`;
+  console.log(apiUrl)
 
-  const response = await fetch(apiUrl, { cache: "no-cache" });
-  const data = response.json();
-  return data;
+  const response = await axios.get(apiUrl);
+  return response.data;
 }
 
-const Users = async () => {
+const Hosts = async () => {
   const data = await getData();
+  const apiStatus = data ? "success" : "error";
   return (
     <div className="p-5">
       <h2 className="text-2xl font-bold">Hosts - {data.length}</h2>
-      {!data && <div>Loading...</div>}
-      {data && <DataTable columns={hostColumns} data={data} />}
+      {apiStatus === "error" && <div>Loading...</div>}
+      {apiStatus === "success" && <DataTable columns={hostColumns} data={data} />}
     </div>
   );
 };
 
-export default Users;
+export default Hosts;
